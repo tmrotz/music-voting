5f656f323195d312870ce8034e95220a54f842d4
Travis Rotz <tmrotz@gmail.com>
2024-07-17 16:08:28 -0500

    Finish basic htmx features

--- app.py
+++ app.py
@@ -14,17 +14,34 @@ app.register_blueprint(spotify.bp)
 def index():
     return "<p>Hello, World!</p>"
 
-@app.route("/songs")
+@app.route("/songs", methods=['GET'])
 def songs():
     search = request.args.get("q")
     page = int(request.args.get("page", 1))
     if search is not None:
         songs_set = Song.search(search)
         if request.headers.get('HX-Trigger') == 'search':
-            return render_template("songs/rows.html", songs=songs_set)
+            return render_template("songs/rows.html", songs=songs_set, page=page)
     else:
         songs_set = Song.all(page)
     return render_template("songs/index.html", songs=songs_set, page=page)
+
+@app.route("/songs", methods=['DELETE'])
+def songs_delete_all():
+    song_ids = [
+        int(id)
+        for id in request.form.getlist("selected_song_ids")
+    ]
+    for song_id in song_ids:
+        song = Song.find(song_id)
+        song.delete()
+    flash("Deleted Songs!")
+    return render_template("songs/index.html", songs=Song.all(), page=1)
+
+@app.route("/songs/count")
+def songs_count():
+    count = Song.count()
+    return f"({count} total Songs)"
 
 @app.route("/songs/new", methods=['GET'])
 def new_song():
@@ -71,8 +88,11 @@ def songs_edit_post(song_id=0):
 def songs_delete(song_id=0):
     song = Song.find(song_id)
     song.delete()
-    flash("Deleted Song!")
-    return redirect("/songs", 303)
+    if request.headers.get("HX-Trigger") == 'delete-btn':
+        flash("Deleted Song!")
+        return redirect("/songs", 303)
+    else:
+        return ""
 
 @app.route("/songs/<song_id>/uri", methods=['GET'])
 def songs_uri_get(song_id=0):
